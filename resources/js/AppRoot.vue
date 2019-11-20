d09fe0c99f26661df20c169f0a4746656865e8d5
MarcelArt <marcel.aritonang@gmail.com>
2019-11-20 12:08:45 +0700

    bikin2 approot sama ruter js

---
+++ resources/js/AppRoot.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <v-app>
+        <router-view>
+
+        </router-view>
+      </v-app>
+      
+  </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('hello worold')
+    }
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+import Vuetify from 'vuetify'
+import axios from 'axios'
+import router from './router'
+
+import AppRoot from './AppRoot.vue'
+const app = new Vue({
+    el: '#app',
+    router,
+    components: {
+        AppRoot
+    }
+});
